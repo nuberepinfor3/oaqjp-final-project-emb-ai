02c26b29c79ab61189811662f3edf0c93fd9ccab
nuberepinfor3 <nuberepinfor3@gmail.com>
2025-03-21 19:35:02 -0400

    Formatear salida de la funcion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,36 @@
-import requests  
+import requests
+import json  
 
 def emotion_detector (text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} 
     myobj = { "raw_document": { "text": text_to_analyze } }  
     response = requests.post(url, json = myobj, headers=header)  
-    return response.text
+    formatted_response = json.loads(response.text)
+    
+    # Extraer las puntuaciones de las emociones
+    emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
+    anger_score = emotion_predictions['anger']
+    disgust_score = emotion_predictions['disgust']
+    fear_score = emotion_predictions['fear']
+    joy_score = emotion_predictions['joy']
+    sadness_score = emotion_predictions['sadness']
+    
+    # Determinar la emoción dominante
+    emotions = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
